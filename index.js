4a686501dae5e71a281b3a73acbc5f379f42e664
Rifat Nabi <to.rifat@gmail.com>
2014-04-02 08:17:19 +0600

    added json server suport

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// TODO: add port as passable
-
-var chalk = require('chalk'),
+var fs      = require('fs'),
+    q       = require('q'),
+    chalk   = require('chalk'),
     program = require('commander'),
     connect = require('connect'),
-    spawn = require('child_process').spawn,
-    karma = require('karma').server;
+    karma   = require('karma');
 
 var conf = require('./package.json');
 
@@ -12,10 +11,59 @@ program
    .version(conf.version);
 
 program
-  .command('server')
+  .command('server <port>')
   .description('AngularJS Verifier Server')
-  .action(function(){
+  .action(function(port){
 
+    var appPort = 3000;
+
+    var appServer = connect()
+      // .use(connect.logger('dev'))
+      .use(connect.static(process.cwd()), {index: 'index.html'})
+      .listen(appPort);
+
+    var options = {};
+    options.basePath = process.cwd() + '/';
+    options.files = ['tests.js'];
+
+    options.reporters = ['json'];
+    options.singleRun = false;
+
+    options.proxies = {
+        '/': 'http://localhost:' + appPort + '/'
+    };
+
+    options.urlRoot = '/__karma__/';
+
+    options.configFile = __dirname + '/karma.conf.js';
+
+    karma.server.start(options);
+
+    var app = connect()
+      // .use(connect.logger('dev'))
+      .use(connect.bodyParser())
+      .use(function(req, res) {
+
+        // TODO: fix this dirty hack
+        var content = '';
+        process.stdout.write = function (data) {
+          content += data;
+          return;
+        };
+
+        var tests = req.body.tests,
+            solution = req.body.solution;
+
+        var p1 = fs.writeFile('index.html', solution),
+            p2 = fs.writeFile('tests.js', tests);
+
+        q.all([p1, p2]).then(function () {
+          karma.runner.run(options, function (exitCode) {
+            res.end(content);
+          });
+        });
+      })
+      .listen(port);
   });
 
 program
@@ -23,12 +71,10 @@ program
   .description('AngularJS Verifier')
   .action(function(appFile, testFile){
 
-    // console.log(chalk.bgGreen.black('App File: %s'), appFile);
-    // console.log(chalk.bgBlue.black('Test File: %s'), process.cwd() + '/' + testFile);
     var port = 3000;
 
     var app = connect()
-      .use(connect.logger('dev'))
+      // .use(connect.logger('dev'))
       .use(connect.static(process.cwd()), {index: appFile})
       .listen(port);
 
@@ -36,6 +82,8 @@ program
     options.basePath = process.cwd() + '/';
     options.files = [];
     options.files.push(testFile);
+
+    options.singleRun = true;
 
     options.proxies = {
         '/': 'http://localhost:' + port + '/'
@@ -45,7 +93,9 @@ program
 
     options.configFile = __dirname + '/karma.conf.js';
 
-    karma.start(options);
+    karma.server.start(options, function (exitCode) {
+      process.exit(exitCode);
+    });
   });
 
  program.parse(process.argv);
