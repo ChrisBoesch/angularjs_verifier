c1ab3d62bc9c18a4b47fd447cb0436f34e960ee4
Rifat Nabi <to.rifat@gmail.com>
2014-04-02 06:04:33 +0600

    added single run cli support

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// TODO: add port as passable
+
 var chalk = require('chalk'),
     program = require('commander'),
     connect = require('connect'),
-    http = require('http'),
     spawn = require('child_process').spawn,
     karma = require('karma').server;
 
@@ -15,20 +16,36 @@ program
   .description('AngularJS Verifier Server')
   .action(function(){
 
-    //karma.start();
-
-    connect()
-      .use(connect.logger('dev'))
-      .use(connect.static(process.cwd()))
-      .listen(3000);
   });
 
 program
   .command('*')
   .description('AngularJS Verifier')
   .action(function(appFile, testFile){
-    console.log(chalk.bgGreen.black('App File: %s'), appFile);
-    console.log(chalk.bgBlue.black('Test File: %s'), testFile);
+
+    // console.log(chalk.bgGreen.black('App File: %s'), appFile);
+    // console.log(chalk.bgBlue.black('Test File: %s'), process.cwd() + '/' + testFile);
+    var port = 3000;
+
+    var app = connect()
+      .use(connect.logger('dev'))
+      .use(connect.static(process.cwd()), {index: appFile})
+      .listen(port);
+
+    var options = {};
+    options.basePath = process.cwd() + '/';
+    options.files = [];
+    options.files.push(testFile);
+
+    options.proxies = {
+        '/': 'http://localhost:' + port + '/'
+    };
+
+    options.urlRoot = '/__karma__/';
+
+    options.configFile = __dirname + '/karma.conf.js';
+
+    karma.start(options);
   });
 
  program.parse(process.argv);
--- karma.conf.js
+++ karma.conf.js
@@ -10,25 +10,25 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['ng-scenario'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      
+
     ],
 
 
     // list of files to exclude
     exclude: [
-      
+
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+
     },
 
 
@@ -62,6 +62,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
